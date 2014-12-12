0ea27f92fb9fa8ba7630bd82bc55e1665e46b77a
king <ceair>
2014-12-12 15:43:29 +0800

    生成雷区

--- WebRoot/js/mine-sweep.js
+++ WebRoot/js/mine-sweep.js
@@ -8,3 +8,69 @@
  * 
  *  =================================================================
  */
+var sideLength = 16;
+var firstXY = {};
+firstXY.x = 50;
+firstXY.y = 40;
+
+$(document).ready(function(){
+	var row = 13, col = 20;
+	for(var i = 0; i < row; i++){
+		if(i % 2 != 0)
+			col = 19;
+		else 
+			col = 18;
+		
+		for(var j = 0; j < col; j++){
+			var centerXY = {};
+			if(i % 2 == 0)
+				centerXY.x = firstXY.x + sideLength * Math.sqrt(3) / 2 + sideLength * Math.sqrt(3) * j;
+			else
+				centerXY.x = firstXY.x + sideLength * Math.sqrt(3) * j;
+			
+			centerXY.y = firstXY.y + sideLength * i * 3 / 2;
+			
+			var tempP = createPolygon(i, centerXY, sideLength);
+			$("#panel").append(tempP);
+		}
+	}
+});
+
+function createPolygon(index, centerXY, sideLen){
+	var x = centerXY.x, y = centerXY.y;
+	var points = [];
+	var polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+	var first = {}, second = {}, third = {}, 
+		fourth = {}, fifth = {}, sixth = {};
+	first.x = x - Math.sqrt(3) * sideLen / 2;
+	first.y = y - sideLen / 2;
+	second.x = x;
+	second.y = y - sideLen;
+	third.x = x + Math.sqrt(3) * sideLen / 2;
+	third.y = y - sideLen / 2;
+	fourth.x = x + Math.sqrt(3) * sideLen / 2;
+	fourth.y = y + sideLen / 2;
+	fifth.x = x;
+	fifth.y = y + sideLen;
+	sixth.x = x - Math.sqrt(3) * sideLen / 2;
+	sixth.y = y + sideLen / 2;
+	
+	points.push(first);
+	points.push(second);
+	points.push(third);
+	points.push(fourth);
+	points.push(fifth);
+	points.push(sixth);
+	
+	var pointStr = "";
+	for(var i = 0; i < points.length; i++){
+		pointStr += points[i].x + "," + points[i].y + " ";
+	}
+	
+	$(polygon).attr("id", index);
+	$(polygon).attr("points", pointStr.trim());
+	return polygon;
+}
+String.prototype.trim = function(){
+    return this.replace(/(^[\s]*)|([\s]*$)/g, "");
+};
